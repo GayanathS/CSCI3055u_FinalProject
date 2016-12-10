21a348094286213c2ed3070e26bf389c2e2e3a5b
alex2639 <mr.e_000@hotmail.com>
2016-12-10 10:57:14 -0500

    Added insertion sort function

--- insertion_sort/src/main.rs
+++ insertion_sort/src/main.rs
@@ -1,3 +1,30 @@
+fn insertionSort(mut array: Vec<i32>) -> Vec<i32>{
+  for i in 1..array.len(){
+    let key = array[i];
+    let mut j=i-1;
+    while j >= 0 && array[j] > key{
+      array[j+1] = array[j];
+      j = j-1;
+    }
+    array[j+1] = key;
+  }
+  array
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut array = vec![1,5,3,22,43,6,2,7];
+    println!("Original array:");
+    for i in array.iter(){
+      print!("{} ",i);
+    }
+    println!("");
+
+    array=insertionSort(array);
+
+    println!("Sorted array:");
+
+    for i in array.iter(){
+      print!("{} ",i);
+    }
+    println!("");
 }
